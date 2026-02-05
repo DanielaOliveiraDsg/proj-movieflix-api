18f6e3490bf0ed446d5f6cfbf4af464dceea52c2
DanielaOliveiraDsg <danielaoliveiradsg@gmail.com>
2026-02-04 16:01:48 -0800

    feat: adding filter by genre

--- src/server.ts
+++ src/server.ts
@@ -20,6 +20,59 @@ app.get("/movies", async (_, res) => {
   res.json(movies);
 });
 
+// filter record - filtering movies by genre
+app.get("/movies/:genre", async (req, res) => {
+  try {
+    const genreType = req.params.genre;
+
+    // 1- check if genre exists
+    const genreExists = await prisma.genre.findFirst({
+      where: {
+        name: {
+          equals: genreType,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (!genreExists) {
+      return res.status(404).send({
+        message: `Genre '${genreType}' not found`,
+      });
+    }
+
+    // 2- fetch movies by genre
+    const moviesByGenre = await prisma.movie.findMany({
+      include: {
+        genres: true,
+        languages: true,
+      },
+      where: {
+        genres: {
+          name: {
+            equals: genreType,
+            mode: "insensitive",
+          },
+        },
+      },
+    });
+
+    // 3- Genre exists, but no movies found in the category
+    // findMany always returns an array by design. So testing its length we can find out if there is any movies at all
+    if (moviesByGenre.length === 0) {
+      return res.status(404).send({
+        message: `No movies found for genre '${genreType}'`,
+      });
+    }
+
+    // 4- success = returns the array of movies
+    res.status(200).send(moviesByGenre);
+
+  } catch (error) {
+    res.status(500).send({ message: "Fail to filter movies by genre" });
+  }
+});
+
 //create a new record
 app.post("/movies", async (req, res) => {
   const { title, genre_id, language_id, oscar_count, release_date } = req.body;
@@ -55,23 +108,25 @@ app.post("/movies", async (req, res) => {
 //updating a record
 app.put("/movies/:id", async (req, res) => {
   //get the register id to be updated
-  const id = Number(req.params.id)
+  const id = Number(req.params.id);
 
   //get record data to be updated and update in Prisma
-  const data = {...req.body}
-  data.release_date = data.release_date ? new Date(data.release_date) : undefined
+  const data = { ...req.body };
+  data.release_date = data.release_date
+    ? new Date(data.release_date)
+    : undefined;
 
   try {
     const movie = await prisma.movie.findUnique({
-      where: {id}
-    })
+      where: { id },
+    });
 
     if (!movie) {
       return res.status(404).send({ message: "Movie record doesn't exist" });
     }
     await prisma.movie.update({
-      where: {id},
-      data
+      where: { id },
+      data,
     });
   } catch (error) {
     return res.status(500).send({ message: "Fail to update movie" });
@@ -80,26 +135,26 @@ app.put("/movies/:id", async (req, res) => {
 });
 
 // deleting a record
-app.delete("/movies/:id", async (req , res)=>{
+app.delete("/movies/:id", async (req, res) => {
   //get the register id to be updated
-  const id = Number(req.params.id)
+  const id = Number(req.params.id);
 
   try {
     const movie = await prisma.movie.findUnique({
-      where: {id}
-    })
+      where: { id },
+    });
 
     if (!movie) {
       return res.status(404).send({ message: "Movie record doesn't exist" });
     }
     await prisma.movie.delete({
-      where: {id}
+      where: { id },
     });
   } catch (error) {
     return res.status(500).send({ message: "Fail to delete movie" });
   }
   res.status(201).send();
-})
+});
 
 app.listen(port, () => {
   console.log(`Servidor em execução em http://localhost:${port}`);
